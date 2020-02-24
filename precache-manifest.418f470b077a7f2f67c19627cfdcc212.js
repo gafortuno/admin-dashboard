9e0c55577b1dc671c1425102b05a518d8d05d819
Gio Adrian Fortuno <fortunogioadrian@gmail.com>
2020-02-24 17:10:35 +0800

    Updates

---
+++ precache-manifest.418f470b077a7f2f67c19627cfdcc212.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595abc07435a52711629886edfa15615",
+    "url": "/admin-dashboard/index.html"
+  },
+  {
+    "revision": "b0aa411a0dae9f763cc4",
+    "url": "/admin-dashboard/static/css/main.924aea6c.chunk.css"
+  },
+  {
+    "revision": "6754bc32b56492d52a09",
+    "url": "/admin-dashboard/static/js/2.e222b734.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/admin-dashboard/static/js/2.e222b734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0aa411a0dae9f763cc4",
+    "url": "/admin-dashboard/static/js/main.6118df69.chunk.js"
+  },
+  {
+    "revision": "89ed30f7be7c629892f1",
+    "url": "/admin-dashboard/static/js/runtime-main.bbe14133.js"
+  },
+  {
+    "revision": "622cbf0d535462fe561b23da79c4c4bb",
+    "url": "/admin-dashboard/static/media/display_pic.622cbf0d.jpg"
+  },
+  {
+    "revision": "18a643fba42c04fe154de3b63d6f2e71",
+    "url": "/admin-dashboard/static/media/logo.18a643fb.png"
+  }
+]);
